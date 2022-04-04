fac24a702dfa58d04204448e3b38181f27d4c3ef
GualaBanana <tem4rik@gmail.com>
2022-04-04 15:53:09 +0300

    Add LinkedList.Node.

---
+++ LinkedList.cs
@@ -0,0 +1,21 @@
+﻿namespace Collections;
+
+
+public class LinkedList
+{
+    public class Node
+    {
+        int _value;
+        Node? _next;
+
+        public ref int Value => ref _value;
+        public ref Node? Next => ref _next;
+        public Node DeepCopy => new(_value, _next);
+
+        public Node(int value, Node? next = null)
+        {
+            _value = value;
+            _next = next;
+        }
+    }
+}
