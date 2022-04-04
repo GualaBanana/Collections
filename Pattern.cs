0aa4fb0112433ff76392d5629377917a39d56533
GualaBanana <tem4rik@gmail.com>
2022-04-04 20:55:22 +0300

    Add Patterns.

---
+++ Pattern.cs
@@ -0,0 +1,19 @@
+﻿namespace Collections;
+
+public enum Pattern
+{
+    Fill,
+    Ascending,
+    Descending,
+}
+
+public static class PatternExtensions
+{
+    public static IEnumerable<int> GetEnumerable(this Pattern pattern, int seed) => pattern switch
+    {
+        Pattern.Fill => Enumerable.Repeat(seed, int.MaxValue),
+        Pattern.Ascending => Enumerable.Range(seed, int.MaxValue),
+        Pattern.Descending => Enumerable.Range(seed, 1),
+        _ => throw new NotImplementedException()
+    };
+}
